1ed252ef8027a3b794fbdf25c337b4d25c6574a5
davidAdeshinaArungbemi <daviizgbemi@gmail.com>
2023-09-26 13:50:04 +0100

    no message

---
+++ build/OurDataframe/CMakeFiles/ODf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ODf.dir/ODf.cpp.o"
+  "CMakeFiles/ODf.dir/ODf.cpp.o.d"
+  "libODf.dylib"
+  "libODf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ODf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/OurDataframe/CMakeFiles/ODf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ODf.
+# This may be replaced when dependencies are built.
